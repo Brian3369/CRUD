5a8e2a7836372eeb0adbd00155387c43b8febbbf
Brian Alexander Tavarez Duarte <briam1952@gmail.com>
2025-04-09 01:12:21 -0400

    Update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $conexion = new Conexion();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CRUD PHP</title>
+  <title>CRUD php</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-DQvkBjpPgn7RC31MCQoOeC9TI2kdqa4+BSgNMNj8v77fdC77Kj5zpWFTJaaAoMbC" crossorigin="anonymous">
 </head>
 
